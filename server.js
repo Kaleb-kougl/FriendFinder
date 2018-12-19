d496d310b18b2d78cfeea1f44373dd98aeac7f4a
Kaleb Kougl <kalebkougl@gmail.com>
2018-12-19 16:30:00 -0600

    adding server.js, hooking up html pages

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+// var path = require("path");
+// var mysql = require("mysql");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// var apiRoute = require("./app/routes/apiRoutes")
+
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
