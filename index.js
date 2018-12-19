53919f5ce3e111989362accd434f169b1e06e76b
Kaleb Kougl <kalebkougl@gmail.com>
2018-12-18 19:56:02 -0600

    hooking up to mysql db on local server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require("path");
+var mysql = require("mysql");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,15 +10,26 @@ app.use(express.json());
 
 var connection = mysql.createConnection({
   host: "localhost",
+
+  // Your port; if not 3306
   port: 8889,
+
+  // Your username
   user: "root",
+
+  // Your password
   password: "root",
-  database: "inventory"
+  database: "bamazon",
+  multipleStatements: true
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    //once successfully connected, you may want to query your database for the info you'll need later!
-  }
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
 });
+
+function afterConnection() {
+  console.log('not broken');
+  connection.end();
+}
